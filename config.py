2646638471a1f7fe625b791f7c0b663c85cc2b7e
Eduardo Leal <49756870+OnlyEduardo@users.noreply.github.com>
2020-03-06 09:43:14 -0300

    Otimização de código
    Removi repetições desnecessárias que poluíam o código.

--- config.py
+++ config.py
@@ -32,16 +32,19 @@ exemplo_matriz5 = '\n\n' \
                   '(a41, a42, a43, a44, a45)\n' \
                   '(a51, a52, a53, a54, a55)\n'
 
+exemplo_matriz6 = '\n\n' \
+                  '(a11, a12, a13, a14, a15, a16)\n' \
+                  '(a21, a22, a23, a24, a25, a26)\n' \
+                  '(a31, a32, a33, a34, a35, a36)\n' \
+                  '(a41, a42, a43, a44, a45, a46)\n' \
+                  '(a51, a52, a53, a54, a55, a56)\n' \
+                  '(a61, a62, a63, a64, a65, a66)\n'
+
+lista_matrizes = (exemplo_matriz2, exemplo_matriz3, exemplo_matriz4, exemplo_matriz5, exemplo_matriz6)
 
 def ex_matriz(n):
-    if n == 2:
-        print("Considere que a matriz terá esse formato: {}".format(exemplo_matriz2))
-    elif n == 3:
-        print("Considere que a matriz terá esse formato: {}".format(exemplo_matriz3))
-    elif n == 4:
-        print("Considere que a matriz terá esse formato: {}".format(exemplo_matriz4))
-    elif n == 5:
-        print("Considere que a matriz terá esse formato: {}".format(exemplo_matriz5))
+    n -= 2
+    print("Considere que a matriz terá esse formato: {}".format(lista_matrizes[n]))
 
 
 def determinante(matriz_a):
@@ -61,110 +64,51 @@ def determinante(matriz_a):
 def define_matriz():
     while True:
         try:
-            tamanho_da_matriz = int(input("Qual o tamanho da matriz nxn ? (entre 2 até 5) "))
-            if tamanho_da_matriz <= 1:
+            tamanho_da_matriz = int(input("Qual o tamanho da matriz nxn ? (entre 2 até 6) "))
+
+            if tamanho_da_matriz <= 1 or tamanho_da_matriz >= 7:
                 tamanho_da_matriz = 2
-                print("Devido ao valor digitado ser inválido\n"
-                      "A matriz será considerada {}".format(tamanho_da_matriz))
-                return tamanho_da_matriz
-            elif tamanho_da_matriz >= 6:
-                tamanho_da_matriz = 5
-                print("Devido ao valor digitado ser inválido\n"
-                      "A matriz será considerada {}".format(tamanho_da_matriz))
-                return tamanho_da_matriz
-            else:
-                return tamanho_da_matriz
+                print("Devido ao valor digitado ser inválido, a matriz será considerada {}x{}".format(tamanho_da_matriz,tamanho_da_matriz))
+
+            ex_matriz(tamanho_da_matriz)
+
+            return tamanho_da_matriz
         except ValueError:
             print("Valor digitado inválido")
 
 
 def pega_matriz():
-    matriz = define_matriz()
+    tamanho_matriz = define_matriz()
     while True:
         try:
-            if matriz == 2:
-                ex_matriz(matriz)
-                a11 = int(input("Digite valor de a11: "))
-                a12 = int(input("Digite valor de a12: "))
-                a21 = int(input("Digite valor de a21: "))
-                a22 = int(input("Digite valor de a22: "))
 
-                return [(a11, a12),
-                        (a21, a22)]
+            nn = tamanho_matriz ** 2
 
-            elif matriz == 3:
-                ex_matriz(matriz)
-                a11 = int(input("Digite valor de a11: "))
-                a12 = int(input("Digite valor de a12: "))
-                a13 = int(input("Digite valor de a13: "))
-                a21 = int(input("Digite valor de a21: "))
-                a22 = int(input("Digite valor de a22: "))
-                a23 = int(input("Digite valor de a23: "))
-                a31 = int(input("Digite valor de a31: "))
-                a32 = int(input("Digite valor de a32: "))
-                a33 = int(input("Digite valor de a33: "))
+            matriz = []
+            temp_list = []
 
-                return [(a11, a12, a13),
-                        (a21, a22, a23),
-                        (a31, a32, a33)]
+            volta = 0
+            linha = 1
+            coluna = 1
 
-            elif matriz == 4:
-                ex_matriz(matriz)
-                a11 = int(input("Digite valor de a11: "))
-                a12 = int(input("Digite valor de a12: "))
-                a13 = int(input("Digite valor de a13: "))
-                a14 = int(input("Digite valor de a14: "))
-                a21 = int(input("Digite valor de a21: "))
-                a22 = int(input("Digite valor de a22: "))
-                a23 = int(input("Digite valor de a23: "))
-                a24 = int(input("Digite valor de a24: "))
-                a31 = int(input("Digite valor de a31: "))
-                a32 = int(input("Digite valor de a32: "))
-                a33 = int(input("Digite valor de a33: "))
-                a34 = int(input("Digite valor de a34: "))
-                a41 = int(input("Digite valor de a41: "))
-                a42 = int(input("Digite valor de a42: "))
-                a43 = int(input("Digite valor de a43: "))
-                a44 = int(input("Digite valor de a44: "))
+            for i in range(nn+1):
+                
+                if volta == tamanho_matriz:
+                    volta = 0
+                    linha += 1
+                    coluna = 1
 
-                return [(a11, a12, a13, a14),
-                        (a21, a22, a23, a24),
-                        (a31, a32, a33, a34),
-                        (a41, a42, a43, a44)]
+                    matriz.append(temp_list)
+                    temp_list = []
 
-            elif matriz == 5:
-                ex_matriz(matriz)
-                a11 = int(input("Digite valor de a11: "))
-                a12 = int(input("Digite valor de a12: "))
-                a13 = int(input("Digite valor de a13: "))
-                a14 = int(input("Digite valor de a14: "))
-                a15 = int(input("Digite valor de a15: "))
-                a21 = int(input("Digite valor de a21: "))
-                a22 = int(input("Digite valor de a22: "))
-                a23 = int(input("Digite valor de a23: "))
-                a24 = int(input("Digite valor de a24: "))
-                a25 = int(input("Digite valor de a25: "))
-                a31 = int(input("Digite valor de a31: "))
-                a32 = int(input("Digite valor de a32: "))
-                a33 = int(input("Digite valor de a33: "))
-                a34 = int(input("Digite valor de a34: "))
-                a35 = int(input("Digite valor de a34: "))
-                a41 = int(input("Digite valor de a41: "))
-                a42 = int(input("Digite valor de a42: "))
-                a43 = int(input("Digite valor de a43: "))
-                a44 = int(input("Digite valor de a44: "))
-                a45 = int(input("Digite valor de a45: "))
-                a51 = int(input("Digite valor de a51: "))
-                a52 = int(input("Digite valor de a52: "))
-                a53 = int(input("Digite valor de a53: "))
-                a54 = int(input("Digite valor de a54: "))
-                a55 = int(input("Digite valor de a55: "))
+                if i < nn:
+                    getvalue = int(input("Digite o valor de a{}{}: ".format(linha, coluna)))
+                    temp_list.append(getvalue)
 
-                return [(a11, a12, a13, a14, a15),
-                        (a21, a22, a23, a24, a25),
-                        (a31, a32, a33, a34, a35),
-                        (a41, a42, a43, a44, a45),
-                        (a51, a52, a53, a54, a55)]
+                coluna += 1
+                volta += 1 
+
+            return matriz
 
         except ValueError:
             print("Valor digitado inválido")
